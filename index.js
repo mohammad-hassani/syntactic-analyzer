a2c2fd23354cfa4de9d0b8529f7ffe1d6218cc09
mohammad-hassani <hasani9821.mh@gmail.com>
2022-07-01 12:29:18 +0430

    simple graph

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 $(document).ready(function(){
-
     // jQuery methods go here...
 
+    function cha() {
+        var root = am5.Root.new("chartdiv");
+
+        root.setThemes([
+          am5themes_Animated.new(root)
+        ]);
+    
+        
+        
+        var container = root.container.children.push(
+          am5.Container.new(root, {
+            width: am5.percent(100),
+            height: am5.percent(100),
+            layout: root.verticalLayout
+          })
+        );
+        
+        var series = container.children.push(
+          am5hierarchy.Tree.new(root, {
+            singleBranchOnly: false,
+            downDepth: 1,
+            initialDepth: 5,
+            topDepth: 0,
+            valueField: "value",
+            categoryField: "name",
+            childDataField: "children"
+          })
+        );
+        
+        series.circles.template.setAll({
+          radius: 20
+        });
+        
+        series.outerCircles.template.setAll({
+          radius: 20
+        });
+        
+        series.data.setAll(chartdata);
+        series.set("selectedDataItem", series.dataItems[0]);
+    }
+
+
     var arr = [];
+
+    var chartdata = []
 
     let head
 
@@ -16,14 +59,52 @@ $(document).ready(function(){
     var Y = [["*","F","Y"],[null]];
     var F = [["(","E",")"],["i"]];
 
-    var parseTable = [E,X,T,Y,F];
     
     var stack = [];
 
     // get the first element of the array and start with E to start the parsing
 
+    // recursive function to search all child in chartdata
+    let parent = chartdata
+    function searchChild(val , arr, parent) {
+        $.each(parent, function(i, v) {
+            console.log("pname" + v.name);
+            // console.log("val" + val);
+            if (v.name == val) {
+                for (let j = 0; j < arr.length; j++) {
+                    console.log("......................" + arr[j].name);
+                        v.children.push(arr[j]);
+                }
+            }
+            // console.log("=============>" + v.name);
+            // console.log("======v=======>" + v);
+            // console.log("======v.children=======>" + v.children);
+            // if (parent[i].children) {
+            //     // console.log('pchild: exist');
+            //     if (parent[i].children.length > 0) {
+            //         // console.log('pchild: exist ' + parent[i].children.length);
+            //         for (let j = 0; j < parent[i].children.length; j++) {
+            //             // console.log('pchild: exist ' + parent[i].children[j].name);
+            //             searchChild(val, arr, parent[i].children[j]);
+            //         }
+            //     }
+            // }
+            
+        });
+        $.each(parent, function(i, v) {
+            $.each(v.children, function(j, q) {
+                searchChild(val, arr, q);
+                console.log("===========jq===============>" + q);
+            })
+        });
+    }
+
     stack.push("$");
     stack.push("E");
+    chartdata.push({
+        name: "E",
+        children: []
+    })
 
     function pushStack() {
 
@@ -35,6 +116,15 @@ $(document).ready(function(){
         if(temp === "E"){
             stack.push(E[0][1]); // push X
             stack.push(E[0][0]); // push T
+            // add to chartdata like data
+            searchChild("E", [{
+                name: "X",
+                children: []
+            },{
+                name: "T",
+                children: []
+            }], chartdata);
+
             return "T";
         }
         else if(temp === "X"){
@@ -42,6 +132,16 @@ $(document).ready(function(){
                 stack.push(X[0][2]); // push X
                 stack.push(X[0][1]); // push T
                 stack.push(X[0][0]); // push +
+                // add to chartdata like data
+                searchChild("X", [{
+                    name: "X",
+                    children: []
+                },{
+                    name: "+",
+                },{
+                    name: "T",
+                    children: []
+                }], chartdata);
                 // xbool = true;
                 return "+";
             }
@@ -53,6 +153,15 @@ $(document).ready(function(){
         else if(temp === "T"){
             stack.push(T[0][1]); // push Y
             stack.push(T[0][0]); // push F
+            // find all child with name T and add to it
+            searchChild("T", [{
+                name: "Y",
+                children: []
+            },{
+                name: "F",
+                children: []
+            }], chartdata);
+
             return "F";
         }
         else if(temp === "Y"){
@@ -60,6 +169,16 @@ $(document).ready(function(){
                 stack.push(Y[0][2]); // push Y
                 stack.push(Y[0][1]); // push F
                 stack.push(Y[0][0]); // push *
+                // add to chartdata like data
+                searchChild("Y", [{
+                    name: "Y",
+                    children: []
+                },{
+                    name: "F",
+                    children: []
+                },{
+                    name: "*",
+                }], chartdata);
                 // ybool = true;
                 return "*";
             }
@@ -73,10 +192,25 @@ $(document).ready(function(){
                 stack.push(F[0][2]); // push )
                 stack.push(F[0][1]); // push E
                 stack.push(F[0][0]); // push (
+                // add to chartdata like data
+                searchChild("F", [{
+                    name: "(",
+                },{
+                    name: "E",
+                    children: []
+                },{
+                    name: ")",
+                }], chartdata);
+
                 return "(";
             }
             else if(head === "i"){
                 stack.push(F[1][0]); // push i
+                // add to chartdata like data
+                searchChild("F", [{
+                    name: "i",
+                }], chartdata);
+
                 return "i";
             }
         }
@@ -103,8 +237,8 @@ $(document).ready(function(){
         head = arr[0];
 
 
-        console.log("stack: " + stack);
-        console.log("arr: " + arr);
+        // console.log("stack: " + stack);
+        // console.log("arr: " + arr);
         for (let x = 0; x < arr.length; x++) {
             const element = arr[x];
             head = arr[x];
@@ -113,36 +247,16 @@ $(document).ready(function(){
             while(element !== temp)
             {
                 if(temp !== "wrong") {
-                    console.log("stack: " + stack);
-                    console.log("element: " + element);
-                    console.log("temp: " + temp);
+                    // console.log("stack: " + stack);
+                    // console.log("element: " + element);
+                    // console.log("temp: " + temp);
                     temp = pushStack();
                 }
                 else{
-                    console.log("wrong");
+                    // console.log("wrong");
                     break;
                 }
-            }
-            // for (let index = 0; index < 100; index++) {
-            //     console.log("index: " + index);
-            //     if(element !== temp){
-            //         console.log("stack: " + stack);
-            //         console.log("element: " + element);
-            //         console.log("temp: " + temp);
-            //         temp = pushStack();
-            //         // create p and appent to div with id div
-            //     }
-            //     else{
-            //         console.log("befor" + stack);
-            //         stack.pop();
-            //         console.log("after" + stack);
-            //         // temp = pushStack();
-            //         let p = document.createElement("p");
-            //         p.innerHTML = temp;
-            //         document.getElementById("div").appendChild(p);
-            //         index = 100;
-            //     }
-            // }  
+            } 
             if(temp === "wrong") {
                 let p = document.createElement("p");
                 p.innerHTML = temp;
@@ -155,9 +269,9 @@ $(document).ready(function(){
                 break;
             }
             if(element === temp) {
-                console.log("befor" + stack);
+                // console.log("befor" + stack);
                 stack.pop();
-                console.log("after" + stack);
+                // console.log("after" + stack);
                 // temp = pushStack();
                 let p = document.createElement("p");
                 p.innerHTML = temp;
@@ -174,17 +288,8 @@ $(document).ready(function(){
     $('#sub').click(function(){
         
         testString()
+        cha();
 
     });
-    // stack.pop();
-    // stack.push("X");
-    // stack.push("T");
-
-    // stack.pop();
-    // stack.push("Y");
-    // stack.push("F");
-
-    // stack.pop();
-
   
   });
